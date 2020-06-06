1d8e668630c6b76b23e58fb23669a9d8ebda9109
Codesmaster-Lab <codesmaster404@gmail.com>
2020-06-06 13:08:37 +0530

    second

--- CHIFR.cpp
+++ CHIFR.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
-#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define IOS ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL)
 
 int main()
 {
